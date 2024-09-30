68b91e77d9fd2ea59588336595decd1cad02c1a6
F2LX <felixfebryan16@gmail.com>
2024-09-30 18:27:27 +0700

    set middleware"

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'auth' => auth::class
-        ]); 
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
